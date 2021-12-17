8947cd6442c857153667b69f320af5735bf9e73d
Khaled Saidi <khaled.saidi@medisinn.com>
2021-12-17 15:54:54 +0100

    test done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Card from "./components/Card/Card";
+import SearchBar from "./components/SearchBar/SearchBar";
+import data from "./dataset.json";
+import logo from './logo.png';
+
+const center = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+};
 
 function App() {
+  const [search, setSearch] = useState("");
+  const drugs = data.drugs;
+  const filteredDrugs = drugs.filter(
+    ({ name, diseases }) =>
+      name.toLocaleLowerCase().includes(search.toLocaleLowerCase()) ||
+      diseases.some((disease) =>
+        disease.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+      )
+  );
+
+  useEffect(() => {}, [search]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={center}>
+      <div style={{width: '34rem', padding: '30px 0', display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+        <img src={logo} alt="" height={96} width={96} />
+        <a href="/">External link</a>
+      </div>
+      <SearchBar setSearch={setSearch}></SearchBar>
+      <div style={{ width: "34rem", padding: "10px 0" }}>
+        Showing {filteredDrugs.length} results
+      </div>
+      {filteredDrugs.map((drug) => {
+        return <Card key={drug.id} drug={drug}></Card>;
+      })}
     </div>
   );
 }
